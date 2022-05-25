69cd7076ba5971d124e532465f6ece9a3b51a440
Kirill <mihailinkirill@gmail.com>
2022-05-25 14:02:18 +0300

    +first_bot

---
+++ bot.py
@@ -0,0 +1,21 @@
+
+# from urllib.parse import uses_relative
+# from telegram.ext import Updater
+# from telegram.ext import CommandHandler
+# from settings import TG_TOKEN, TG_API_URL
+
+# def sms(bot, update):
+#     print('Кто-то отправил /start. Что делать?')
+#     bot.message.reply_text('Hello, im bot! Social credits')
+#     print(bot.message)
+
+# def main():
+#     my_bot = Updater (TG_TOKEN, TG_API_URL)
+    
+#     my_bot.dispatcher.add_handler(CommandHandler('start', sms))
+
+#     my_bot.start_polling
+#     my_bot.idle()
+
+# print('hello')
+# main()
